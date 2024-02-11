73a7d661083214d4bb597d6278dfeec216158896
Roman Dmytriv <roman-dmytriv@Romans-MacBook-Pro-14.local>
2024-02-12 00:04:25 +0200

    Flake8 oauth.py

--- app/auth/oauth.py
+++ app/auth/oauth.py
@@ -1,6 +1,5 @@
-from datetime import datetime
 import os
-from flask import Blueprint, redirect, request, jsonify, make_response, session, url_for
+from flask import Blueprint, redirect, request, jsonify, make_response, session
 from slack_sdk.oauth import AuthorizeUrlGenerator
 from slack_sdk.oauth.installation_store import FileInstallationStore
 from slack_sdk.errors import SlackApiError
@@ -21,6 +20,7 @@ oauth_redirect_url = "https://581b-45-12-26-219.ngrok-free.app/auth/callback"
 # Initialize Slack WebClient with the Slack API token from the configuration
 slack_client = WebClient(token=Config.SLACK_API_TOKEN)
 
+
 @slack_auth_bp.route('/auth/authorize', methods=['GET'])
 def slack_auth():
     # Generate a random state and store it in the session
@@ -35,13 +35,15 @@ def slack_auth():
     authorization_url = authorize_url_generator.generate(state=state)
     return redirect(authorization_url)
 
+
 @slack_auth_bp.route('/auth/callback', methods=['GET'])
 def slack_auth_callback():
     try:
         # Verify state to prevent CSRF attacks
         state = session.pop('slack_auth_state', None)
         if state is None or state != request.args.get('state'):
-            return make_response(jsonify({"error": "Invalid state parameter."}), 400)
+            return make_response(
+                jsonify({"error": "Invalid state parameter."}), 400)
 
         # Verify and exchange authorization code for access token
         auth_result = slack_client.oauth_v2_access(
@@ -51,7 +53,9 @@ def slack_auth_callback():
         )
 
         if auth_result is None:
-            return make_response(jsonify({"error": "Authentication failed: No response from Slack API."}), 500)
+            return make_response(jsonify({
+                "error": "Authentication failed: No response from Slack API."
+            }), 500)
 
         if isinstance(auth_result, SlackResponse) and auth_result["ok"]:
             # Store the installation data (access token, bot user ID, etc.)
@@ -61,20 +65,30 @@ def slack_auth_callback():
                 bot_token=auth_result['access_token'],
             )
             installation_store.save(installation)
-            
+
             # Respond with a success message
-            return make_response(jsonify({"message": "Authentication successful! You can now use the Slack API."}), 200)
+            return make_response(jsonify({
+                "message": "Authentication successful! You can now use the Slack API."  # noqa
+            }), 200)
 
         elif isinstance(auth_result, dict) and 'error' in auth_result:
-            return make_response(jsonify({"error": f"Authentication failed: {auth_result['error']}"}), 500)
+            return make_response(jsonify({
+                "error": f"Authentication failed: {auth_result['error']}"
+            }), 500)
 
         else:
-            return make_response(jsonify({"error": "Authentication failed: Unexpected response from Slack API."}), 500)
+            return make_response(jsonify({
+                "error": "Authentication failed: Unexpected response from Slack API."   # noqa
+            }), 500)
 
     except SlackApiError as e:
         # Handle Slack API errors
         error_message = e.response['error']
-        return make_response(jsonify({"error": f"Slack API Error: {error_message}"}), 500)
+        return make_response(jsonify({
+            "error": f"Slack API Error: {error_message}"
+        }), 500)
 
     except Exception as e:
-        return make_response(jsonify({"error": f"Internal Server Error: {str(e)}"}), 500)
+        return make_response(jsonify({
+            "error": f"Internal Server Error: {str(e)}"
+        }), 500)
